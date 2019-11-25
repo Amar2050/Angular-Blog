b4d5fb0416780e7cc5e3347ae6f94e9d1a794829
Amar2050 <amarboutayeb@gmail.com>
2019-11-25 20:03:21 +0100

    Update post.model.ts

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,6 +1,7 @@
 export class Post {
   loveIts: number;
-  constructor(public title: string, public content: string, public  createdAt: string) {
+  constructor(public title: string, public content: string, 
+    public  createdAt: string) {
     this.loveIts = 0;
   }
 }
